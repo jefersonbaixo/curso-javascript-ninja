d78066f5c203ea31eadad2b91c3b5c5ebfc6f42c
Jeferson Baixo <jeferson.baixo@gmail.com>
2019-05-06 22:27:06 -0300

    Colocando variaveis nas impressões

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -93,7 +93,8 @@ Ainda com a função acima, imprima a quantidade de páginas de um livro qualque
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-console.log('O "livro Anjos e Demonios" tem ' + book('Anjos e Demonios').quantidadedePaginas + ' páginas!');
+var bookName = 'Anjos e Demonios';
+console.log('O livro "'+ bookName + '" tem ' + book(bookName).quantidadedePaginas + ' páginas!');
 
 
 /*
@@ -101,7 +102,7 @@ Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-console.log('O autor do livro "Anjos e Demonios" é ' + book('Anjos e Demonios').autor + '.');
+console.log('O autor do livro "'+ bookName + '" é ' + book(bookName).autor + '.');
 
 
 /*
@@ -109,4 +110,5 @@ Ainda com a função acima, imprima o nome da editora de um livro qualquer, usan
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-console.log('O livro "O Simbolo Perdido" foi publicado pela editora '+ book('O Simbolo Perdido').editora + '.');
+bookName = 'O Simbolo Perdido';
+console.log('O livro "'+ bookName + '" foi publicado pela editora ' + book(bookName).editora + '.');
